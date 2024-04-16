d12cafca07d7a4dd123c02ed5f0414d07e0ccef7
ej <thehumanej@gmail.com>
2024-04-15 20:23:34 -0500

    first commit

---
+++ src/client/java/e/hajcraft/HajcraftClient.java
@@ -0,0 +1,10 @@
+package e.hajcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HajcraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/e/hajcraft/HajcraftDataGenerator.java
@@ -0,0 +1,11 @@
+package e.hajcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HajcraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
